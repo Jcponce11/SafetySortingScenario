cc893ea2b7d4cb6fc5fa4473670a3e90c579fd88
Jason Ponce <jcponce11@gmail.com>
2020-02-14 17:04:58 -0600

    Implemented VRTK Interations and added new Canvas activating feature

---
+++ Assets/CanvasClicker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class CanvasClicker : MonoBehaviour
+{
+    private VRTK_InteractableObject thisInteractor;
+    private ProximityActivate thisProximityActivator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisInteractor = GetComponent<VRTK_InteractableObject>();
+        thisProximityActivator = GetComponentInChildren<ProximityActivate>();
+        thisProximityActivator.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (thisInteractor.IsGrabbed())
+        {
+            thisProximityActivator.gameObject.SetActive(true);
+        }
+        else if(thisInteractor.IsGrabbed() == false)
+        {
+            thisProximityActivator.gameObject.SetActive(false);
+        }
+    }
+}
