7345bb6b97a57c88f010a25406238da951c9470c
Jason Ponce <jcponce11@gmail.com>
2020-02-17 23:54:33 -0600

    Refactoring Scoring System

---
+++ Assets/GameObjectChanger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class GameObjectChanger : MonoBehaviour
+{
+    public MeshFilter HighlightObjectAlias;
+    public GameObject orbitObject;
+    private MeshFilter orbitObjectMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HighlightObjectAlias = orbitObjectMesh;
+        
+    }
+
+    void OnTriggerEnter(Collider orbitObjectCollision)
+    {
+        if (orbitObjectCollision.GetComponent<VRTK_InteractableObject>().IsGrabbed())
+        {
+            orbitObjectMesh = orbitObjectCollision.GetComponentInChildren<MeshFilter>();
+            Debug.Log(orbitObjectCollision.name);
+        }
+    }
+}
---
+++ Assets/HighlightObjectChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightObjectChanger : MonoBehaviour
+{
+    public MeshFilter aliasObject;
+    public GameObjectChanger gameObjectPassThrough;
+    public MeshFilter thisMeshFilter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        aliasObject = gameObjectPassThrough.HighlightObjectAlias;
+        thisMeshFilter.mesh = aliasObject.mesh;
+    }
+}
